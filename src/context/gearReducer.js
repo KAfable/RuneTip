fbe098b90a094a520c29479cf8cde9cd22e0bb02
Vyraal1 <kevin.afable@gmail.com>
2020-01-26 21:54:03 -0500

    FEAT reducers

---
+++ src/context/gearReducer.js
@@ -0,0 +1,114 @@
+import {
+  EQUIP_FEET,
+  EQUIP_WEAPON,
+  EQUIP_AMMO,
+  EQUIP_CAPE,
+  EQUIP_HANDS,
+  EQUIP_HEAD,
+  EQUIP_LEGS,
+  EQUIP_NECK,
+  EQUIP_SHIELD,
+  EQUIP_BODY,
+  EQUIP_RING
+} from "./types";
+
+import {
+  ammo,
+  feet,
+  capes,
+  hands,
+  head,
+  legs,
+  necks,
+  shields,
+  body,
+  weapons,
+  rings
+} from "../items";
+
+export default (state, action) => {
+  switch (action.type) {
+    case EQUIP_WEAPON:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          weapon: weapons[action.payload]
+        }
+      };
+    case EQUIP_BODY:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          body: body[action.payload]
+        }
+      };
+    case EQUIP_CAPE:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          cape: capes[action.payload]
+        }
+      };
+    case EQUIP_HANDS:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          hands: hands[action.payload]
+        }
+      };
+    case EQUIP_HEAD:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          head: head[action.payload]
+        }
+      };
+    case EQUIP_LEGS:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          legs: legs[action.payload]
+        }
+      };
+    case EQUIP_SHIELD:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          shield: shields[action.payload]
+        }
+      };
+    case EQUIP_RING:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          ring: rings[action.payload]
+        }
+      };
+    case EQUIP_NECK:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          neck: necks[action.payload]
+        }
+      };
+    case EQUIP_FEET:
+      return {
+        ...state,
+        gear: {
+          ...state.gear,
+          feet: feet[action.payload]
+        }
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/context/types.js
@@ -0,0 +1,11 @@
+export const EQUIP_FEET = "EQUIP_FEET";
+export const EQUIP_WEAPON = "EQUIP_WEAPON";
+export const EQUIP_AMMO = "EQUIP_AMMO";
+export const EQUIP_CAPE = "EQUIP_CAPE";
+export const EQUIP_HANDS = "EQUIP_HANDS";
+export const EQUIP_HEAD = "EQUIP_HEAD";
+export const EQUIP_LEGS = "EQUIP_LEGS";
+export const EQUIP_NECK = "EQUIP_NECK";
+export const EQUIP_SHIELD = "EQUIP_SHIELD";
+export const EQUIP_BODY = "EQUIP_BODY";
+export const EQUIP_RING = "EQUIP_RING";
